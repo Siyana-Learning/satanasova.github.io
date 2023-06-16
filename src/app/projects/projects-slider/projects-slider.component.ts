85de0658890edc32c0aeab910cac519391ee265d
Siyana Atanasova <s.e.atanasowa@gmail.com>
2023-06-16 14:44:59 +0300

    added responsive for slider; fixed text size

--- src/app/projects/projects-slider/projects-slider.component.ts
+++ src/app/projects/projects-slider/projects-slider.component.ts
@@ -17,20 +17,17 @@ export class ProjectsSliderComponent implements OnInit, AfterViewInit {
   @Output() deviceTypeChanged: EventEmitter<DeviceType> = new EventEmitter();
 
   deviceType: DeviceType = DeviceType.PHONE;
-  // resizeObservable: Observable<Event> = fromEvent(window, 'resize');
-  sliderContainerHeight: number = 800;
+
+  sliderContainerHeight: number = 0;
+  sliderContainerWidth: number = 0;
 
   constructor(public colorService: ColorService) { }
 
   ngAfterViewInit(): void {
-    // if(this.sliderContainer){
-    //   this.sliderContainerWidth = this.sliderContainer.nativeElement.clientWidth
-    // }
-
-    // this.resizeObservable.subscribe(() => {
-    //   this.sliderContainerWidth = this.sliderContainer.nativeElement.clientWidth;
-    //   console.log('slider width ON RESIZE',this.sliderContainerWidth);
-    // })
+    if(this.sliderContainer){
+      this.sliderContainerWidth = this.sliderContainer.nativeElement.clientWidth;
+      this.sliderContainerHeight = this.sliderContainer.nativeElement.clientHeight;
+    }
   }
 
   ngOnInit(): void {
@@ -59,47 +56,54 @@ export class ProjectsSliderComponent implements OnInit, AfterViewInit {
   
   getProjectStyling(projects: Project[], i: number) {
     const style: any = {};
-    let transform: string = '';
     let scale: number = 0;
     let left: number = 0;
     let top: number = 0;
-    let activeProjectZoomFactor = 1.3;
-    let rotate: string = '';
-    let defaultDeviceScale = 1;
+    let activeProjectZoomFactor = 1.4;
+    let rotate: number = 0;
     const indexDistance = i - this.activeProjectIndex;
+    const breakpoints: any = {
+      mobile: 425,
+      tablet: 875,
+      laptop: 1366
+    }
 
     switch (this.deviceType.name) {
       case 'phone': scale = 0.55; break;
       case 'tablet': scale = 0.45; break;
-      case 'laptop': scale = 0.35; break;
+      case 'laptop': scale = 0.3; break;
     }
-
-    scale *= 0.5
     
     if (this.activeProjectIndex === i) {
       scale *= activeProjectZoomFactor;
     } 
-    
+
+    if(this.sliderContainerWidth < breakpoints.mobile) {
+      scale *= 0.5;
+    } else if (this.sliderContainerWidth > breakpoints.mobile && this.sliderContainerWidth < breakpoints.tablet) {
+      scale *= 0.7;
+    } 
+
+    rotate = indexDistance * (scale * 0.7) * 50
     left = indexDistance * (this.deviceType.width*scale) - (this.deviceType.width - this.deviceType.width*scale)/2;
     top = -(this.deviceType.height - this.deviceType.height*scale)/2;
 
     if(i > this.activeProjectIndex) {
-      left +=  this.deviceType.width * scale * (activeProjectZoomFactor - 1);
+      left += this.deviceType.width * scale * (activeProjectZoomFactor - 1);
     }
-
-    // if (this.activeProjectIndex !== i) {
-    //   top += (this.deviceType.height * scale * (activeProjectZoomFactor -1))/2
-    // }
 
     top += Math.round((this.sliderContainerHeight - this.deviceType.height*scale)/2);
 
-
-    // cover case for active device
+    if (this.sliderContainerWidth < breakpoints.mobile) {
+      left += 20; 
+    } else {
+      left += this.sliderContainerWidth * 0.2
+    }
   
     style.scale = scale;
     style.left = left +'px';
     style.top = top + 'px';
-    // style.rotate = rotate;
+    style.rotate = rotate + 'deg';
     style.opacity = indexDistance <= 3 ? 1 : 0;
     style.zIndex = 49 - Math.abs(indexDistance * 10)
     style.filter = `blur(${Math.abs(indexDistance)*2}px)`;
@@ -114,22 +118,6 @@ export class ProjectsSliderComponent implements OnInit, AfterViewInit {
   changeDeviceWidth(device: string) {
     this.deviceType = (DeviceType as any)[device];
     this.deviceTypeChanged.emit(this.deviceType as any);
-    // this.deviceWidth = 
-    // console.log(this.deviceWidth);
   }
-
-  getDevicePosition(windowSize: number) {
-    let scale: number = 0;
-    
-    if(windowSize <= 360) {
-      scale = 0.3
-    } else 
-    if(windowSize > 360 && windowSize <= 640) {
-      scale = 0.5
-    }
-
-    // console.log('get scale', scale);
-    
-  }
-
+  
 }
